e8c55803cbbf4b9f57a9f01679e1670ad34e821e
Ben Drucker <bvdrucker@gmail.com>
2019-07-12 10:03:27 -0700

    standard --fix

--- test.js
+++ test.js
@@ -4,10 +4,10 @@ var test = require('tape')
 var filter = require('./')
 
 test(function (t) {
-  t.deepEquals(filter({a: true, b: false, c: 1}), {
+  t.deepEquals(filter({ a: true, b: false, c: 1 }), {
     a: true,
     c: 1
   })
-  t.deepEquals(filter({a: 0, b: false, c: ''}), {})
+  t.deepEquals(filter({ a: 0, b: false, c: '' }), {})
   t.end()
 })
